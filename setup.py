3a09caeefe660a0791dc9a09b1d2d38d1c5bb73e
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 12:21:20 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_reesinkgive',
-    py_modules=['cldfbench_reesinkgive'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'reesinkgive=cldfbench_reesinkgive:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench[glottolog,excel]',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
